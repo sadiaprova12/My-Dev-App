9dbe89797d891862dfa86d06abe1f042f0f202ca
sadiaprova12 <sadiaprova51717@gmail.com>
2024-10-13 00:09:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Bio from "./assets/sections/Bio"
+
+function App() {
+
+  return (
+    <>
+      
+     <div className="container-md mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 p-3 md:p-7">
+      <div className=' '>
+        <Bio/>
+        </div>
+      <div className='border border-bacground col-span-2'>column-2</div>
+     </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/sections/Bio.jsx
@@ -0,0 +1,11 @@
+const Bio = () => {
+    return (
+        <section className="bg-white text-center md:text-left rounded-xl p-2 md:p-5 shadow-md text-black">
+            <h1 className="text-lg md:text-2xl font-bold mt-3">Dev Webdecoded</h1>
+            <p className="text-secondary-foreground py-2">I'm a Junior Full-Stack Engineer.</p>
+        </section>
+    )
+    
+};
+
+export default Bio;
