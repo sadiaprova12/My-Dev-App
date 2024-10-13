973d7a5b451b36519ddcff54cbf61f394e184572
sadiaprova12 <sadiaprova51717@gmail.com>
2024-10-13 21:56:45 +0600

    import skills

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Bio from "./assets/sections/Bio"
+import Skills from './assets/sections/Skills'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
      <div className="container-md mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 p-3 md:p-7">
       <div className=' '>
         <Bio/>
+        <Skills />
         </div>
       <div className='border border-bacground col-span-2'>column-2</div>
      </div>
