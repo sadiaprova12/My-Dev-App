61b7030e31562be1df3fabac7c4bb2101be0fb75
sadiaprova12 <sadiaprova51717@gmail.com>
2024-10-13 21:28:51 +0600

    added profile pic

--- src/assets/sections/Bio.jsx
+++ src/assets/sections/Bio.jsx
@@ -4,14 +4,23 @@ import { FaGithub } from "react-icons/fa";
 
 const Bio = () => {
     return (
-        <section className="bg-white text-center md:text-left rounded-xl p-2 md:p-5 shadow-md text-black">
+        <section className="bg-white text-center md:text-left rounded-xl p-5 md:p-5 shadow-md text-black">
+            <img src="/profile-pic.jpg" 
+            alt="profile pic" 
+            className="h-32 w-32 rounded-full"
+            />
             <h1 className="text-lg md:text-2xl font-bold mt-3">Dev Webdecoded</h1>
             <p className="text-secondary-foreground py-2">I'm a Junior Full-Stack Engineer.</p>
             <div className="flex flex-row gap-2 mt-2">
+                <a href="https://www.linkedin.com/in/sadiaprova14" target="_blank" rel="noopener noreferrer">
             <FaLinkedin className="text-blue-800 hover:text-blue-600 cursor-pointer"/>
+            </a>
+            <a href="https://mail.google.com" target="_blank" rel="noopener noreferrer">
             <SiGmail className="text-red-700 hover:text-red-600 cursor-pointer" />
-            <FaGithub />
-
+            </a>
+            <a href="https://github.com/sadiaprova12" target="_blank" rel="noopener noreferrer">
+            <FaGithub className="text-black hover:text-purple-950 cursor-pointer"/>
+            </a>
             </div>
         </section>
     );
